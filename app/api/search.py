16965a5a1ef4332e10b091fa07ca878ef5072812
KunisovIvan <kunisov.ivan@mail.ru>
2022-10-10 01:11:06 +0600

    add BackgroundTasks

--- app/api/search.py
+++ app/api/search.py
@@ -1,14 +1,11 @@
-import asyncio
-import json
 import uuid
 
-import httpx
 from fastapi import APIRouter
+from fastapi import BackgroundTasks
 
 from app.models import dto
 from app.redis import AsyncRedisConnector
-from app.settings import APP
-from app.utils import post_to_provider, convector
+from app.utils import convector, search_
 
 router = APIRouter()
 
@@ -19,28 +16,9 @@ router = APIRouter()
     summary='Get and save results from providers',
     response_model=dto.SearchRes
 )
-async def search() -> dto.SearchRes:
+async def search(background_tasks: BackgroundTasks) -> dto.SearchRes:
     search_id = str(uuid.uuid4())
-    redis = AsyncRedisConnector()
-    await redis.connect()
-    #
-    async with httpx.AsyncClient() as client:
-        #
-        tasks = []
-        for url in APP.PROVIDER_URLS:
-            tasks.append(asyncio.create_task(post_to_provider(url, client)))
-        #
-        for task in tasks:
-            res = await task
-            value = await redis.get_key(search_id)
-            #
-            data = dto.Results(search_id=search_id).prepare_to_redis(value=value, res=res.json())
-            await redis.set_key(search_id, data.json(), ttl=300)
-    #
-    value = await redis.get_key(search_id)
-    data = dto.Results(search_id=search_id).set_status(value=value, status=dto.Status.completed)
-    await redis.set_key(search_id, data.json(), ttl=300)
-    #
+    background_tasks.add_task(search_, search_id)
     return dto.SearchRes(search_id=search_id)
 
 
--- app/utils.py
+++ app/utils.py
@@ -1,13 +1,38 @@
+import asyncio
 import logging
 from datetime import datetime
 
 import httpx
 
 from app.models import dto
+from app.redis import AsyncRedisConnector
+from app.settings import APP
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler())
+
+
+async def search_(search_id: str):
+    redis = AsyncRedisConnector()
+    await redis.connect()
+    #
+    async with httpx.AsyncClient() as client:
+        #
+        tasks = []
+        for url in APP.PROVIDER_URLS:
+            tasks.append(asyncio.create_task(post_to_provider(url, client)))
+        #
+        for task in tasks:
+            res = await task
+            value = await redis.get_key(search_id)
+            #
+            data = dto.Results(search_id=search_id).prepare_to_redis(value=value, res=res.json())
+            await redis.set_key(search_id, data.json(), ttl=300)
+    #
+    value = await redis.get_key(search_id)
+    data = dto.Results(search_id=search_id).set_status(value=value, status=dto.Status.completed)
+    await redis.set_key(search_id, data.json(), ttl=300)
 
 
 async def post_to_provider(url: str, client: httpx.AsyncClient) -> httpx.Response:
