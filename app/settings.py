6fb25496db5248a8b6f0dbcb041c3a297ec29edf
KunisovIvan <kunisov.ivan@mail.ru>
2022-10-10 11:59:42 +0600

    delete import

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,7 @@ import os
 from typing import List
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings, validator, PostgresDsn, IPvAnyInterface, RedisDsn
+from pydantic import BaseSettings, validator, PostgresDsn, IPvAnyInterface
 
 load_dotenv(verbose=True)
 
